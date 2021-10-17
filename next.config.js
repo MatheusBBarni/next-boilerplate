69bb7fa192fe6141e39052771ce2c18d56d7e6e8
Matheus <matheus@pop-os.localdomain>
2021-10-17 01:21:09 -0300

    feat: added next-pwa

--- next.config.js
+++ next.config.js
@@ -1,3 +1,11 @@
-module.exports = {
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const withPwa = require('next-pwa')
+const isProd = process.env.NODE_ENV === 'production'
+
+module.exports = withPwa({
   reactStrictMode: true,
-}
+  pwa: {
+    dest: 'public',
+    disable: !isProd
+  }
+})
