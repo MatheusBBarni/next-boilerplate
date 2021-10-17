6bb419672679a641a717bd7077487dcb7944833f
Matheus <matheus@pop-os.localdomain>
2021-10-17 01:12:53 -0300

    feat: finished Main component

--- src/components/Main/test.tsx
+++ src/components/Main/test.tsx
@@ -12,4 +12,11 @@ describe('<Main />', () => {
 
     expect(container.firstChild).toMatchSnapshot()
   })
+  it('should render the colors correctly', () => {
+    const { container } = render(<Main />)
+
+    expect(container.firstChild).toHaveStyle({
+      'background-color': '#06092b'
+    })
+  })
 })
